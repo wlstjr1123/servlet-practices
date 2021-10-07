bad45f0b1d36642eb377cbd6f014d7b66c1f0f12
wlstjr1123 <wlstjr1123@naver.com>
2021-10-07 14:05:35 +0900

    helloweb_deployment test

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,27 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8"); // 헤더 설정
+		
+		PrintWriter pw = response.getWriter();
+		pw.print("<h1>Hello World</h1>");
+	}
+
+}
